8ec6477b89583899055ff84301a99f5a9c123aa3
programming353 <58999047+programming353@users.noreply.github.com>
2024-01-22 15:58:17 -0500

    Added shooter prematch command

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -7,21 +7,40 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkBase.ControlType;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
-
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.REVLibError;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkPIDController;
 import com.revrobotics.SparkPIDController.ArbFFUnits;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import frc.lib.controllers.VirtualJoystick;
+import frc.lib.controllers.VirtualXboxController;
+import frc.lib.subsystem.VirtualSubsystem;
+import frc.robot.Constants.OperatorConstants;
 import frc.robot.Constants.ShooterConstants;
 import frc.robot.util.SparkMaxUtil;
+import monologue.Logged;
 
-public class Shooter extends SubsystemBase {
-  private CANSparkMax shooterMain = new CANSparkMax(ShooterConstants.shooterMainId, MotorType.kBrushless);
-  private CANSparkMax shooterFollower = new CANSparkMax(ShooterConstants.shooterFollowerId, MotorType.kBrushless);
+public class Shooter extends VirtualSubsystem implements Logged {
+  private CANSparkMax shooterMain =
+      new CANSparkMax(ShooterConstants.shooterMainId, MotorType.kBrushless);
+  private CANSparkMax shooterFollower =
+      new CANSparkMax(ShooterConstants.shooterFollowerId, MotorType.kBrushless);
+
   private SimpleMotorFeedforward shooterFeedforward =
       new SimpleMotorFeedforward(
           ShooterConstants.shooterKs, ShooterConstants.shooterKv, ShooterConstants.shooterKa);
+  private RelativeEncoder mainShooterEncoder = shooterMain.getEncoder();
+
+  private final SysIdRoutine sysIdRoutine =
+      new SysIdRoutine(
+          new SysIdRoutine.Config(),
+          new SysIdRoutine.Mechanism(
+              (volts) -> shooterMain.setVoltage(volts.in(Units.Volts)), null, this));
 
   private SparkPIDController shooterPID = shooterMain.getPIDController();
 
@@ -30,22 +49,53 @@ public class Shooter extends SubsystemBase {
     shooterMain.restoreFactoryDefaults();
     shooterFollower.follow(shooterMain, true);
     shooterMain.setIdleMode(IdleMode.kCoast);
-    SparkMaxUtil.configureFollower(shooterFollower);
 
+    SparkMaxUtil.configureFollower(shooterFollower);
     shooterPID.setP(ShooterConstants.shooterP);
   }
 
   public void setMotorSpeed(double velocity) {
     double feedForward = shooterFeedforward.calculate(velocity);
-
     shooterPID.setReference(velocity, ControlType.kVelocity, 0, feedForward, ArbFFUnits.kVoltage);
   }
 
-
-
+  public void stopMotors() {
+    shooterMain.set(0);
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+
+  @Override
+  public Command getPrematchCheckCommand(
+      VirtualXboxController controller, VirtualJoystick joystick) {
+    return Commands.sequence(
+        Commands.runOnce(
+            () -> {
+              REVLibError mainMotorError = shooterMain.getLastError();
+              if (mainMotorError != REVLibError.kOk) {
+                addError("Main Shooter motor error: " + mainMotorError.name());
+              } else {
+                addInfo("Main Shooter motor contains no errors");
+              }
+            }),
+        // checks both shooter motors
+        Commands.runOnce(
+            () -> {
+              joystick.setButton(OperatorConstants.shootButton, true);
+            }),
+        Commands.waitSeconds(10),
+        Commands.runOnce(
+            () -> {
+              double velocity = mainShooterEncoder.getVelocity();
+              double velocityDifference = Math.abs(velocity - ShooterConstants.shooterVelocity);
+              if (velocityDifference > 10) {
+                addError("Shooter Motor not near desired velocity");
+              } else {
+                addInfo("Shooter motors at desired velocity");
+              }
+            }));
   }
 }

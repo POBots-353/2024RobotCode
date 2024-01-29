c567ff901f9013b4d71949c0aab76e5489c05dff
programming353 <58999047+programming353@users.noreply.github.com>
2024-01-29 16:48:12 -0500

    Fixed lights

--- src/main/java/frc/robot/commands/leds/FlashingLights.java
+++ src/main/java/frc/robot/commands/leds/FlashingLights.java
@@ -5,20 +5,22 @@
 package frc.robot.commands.leds;
 
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.LEDConstants;
 import frc.robot.subsystems.LEDs;
-import edu.wpi.first.wpilibj.Timer;
 
 public class FlashingLights extends Command {
   private final Color color;
   private final LEDs leds;
   private Timer timer = new Timer();
-  double time;
+
+  private double period;
+  private boolean ledsOn = false;
 
   /** Creates a new FlashingLights. */
-  public FlashingLights(double time,Color color, LEDs leds) {
+  public FlashingLights(double period, Color color, LEDs leds) {
     this.color = color;
     this.leds = leds;
 
@@ -29,30 +31,35 @@ public class FlashingLights extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    timer.start();
+    ledsOn = false;
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    double getTime = timer.get();
-    AddressableLEDBuffer buffer = leds.getBuffer();
-    if (getTime <= time) {
-      for (int i = 0; i < buffer.getLength(); i++) {
-        buffer.setLED(i, color);
+    if (timer.advanceIfElapsed(period)) {
+      AddressableLEDBuffer buffer = leds.getBuffer();
+      ledsOn = !ledsOn;
+      if (ledsOn) {
+        for (int i = 0; i < buffer.getLength(); i++) {
+          buffer.setLED(i, color);
+        }
+      } else {
+        for (int i = 0; i < buffer.getLength(); i++) {
+          buffer.setLED(i, LEDConstants.transparent);
+        }
       }
+
       leds.updateBuffer();
-    } else if(getTime >= 2*time) {
-      timer.restart();
-    } else {
-      for (int i = 0; i < buffer.getLength(); i++){
-      buffer.setLED(i, LEDConstants.transparent);
-      }
     }
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    timer.stop();
+    timer.reset();
   }
 
   // Returns true when the command should end.
